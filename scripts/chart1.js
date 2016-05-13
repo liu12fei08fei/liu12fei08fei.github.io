dbf999791114501aa3ef81436897c9fa75e666ce
liu12fei08fei <759665259@qq.com>
2016-05-13 15:25:21 +0800

    我自己

--- scripts/chart1.js
+++ scripts/chart1.js
@@ -1,5 +1,5 @@
 $(function () {
-    var date = new Date(),year = date.getFullYear(),month = date.getMonth(),day = date.getDate();
+    var date = new Date(),year = date.getFullYear(),month = date.getMonth()+1,day = date.getDate();
     var aDate = year+'-'+month+'-'+day;
     $('#chart1').highcharts({
         chart: {
