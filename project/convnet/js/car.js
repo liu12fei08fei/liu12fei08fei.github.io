df78173ea5b0ac0288e73ea8c2e0dbd2cd12ee0f
刘非非 <759665259@qq.com>
2018-04-18 00:04:15 +0800

    添加一堆东西

---
+++ project/convnet/js/car.js
@@ -0,0 +1,62 @@
+var carList = [
+  {
+    "name": "奥迪",
+    "url": "http://p1fg8xetu.bkt.clouddn.com/car_0.png",
+    "index": 0
+  },
+  {
+    "name": "奔驰",
+    "url": "http://p1fg8xetu.bkt.clouddn.com/car_1.png",
+    "index": 1
+  },
+  {
+    "name": "宝马",
+    "url": "http://p1fg8xetu.bkt.clouddn.com/car_2.png",
+    "index": 2
+  },
+  {
+    "name": "本田",
+    "url": "http://p1fg8xetu.bkt.clouddn.com/car_3.png",
+    "index": 3
+  },
+  {
+    "name": "别克",
+    "url": "http://p1fg8xetu.bkt.clouddn.com/car_4.png",
+    "index": 4
+  },
+  {
+    "name": "比亚迪",
+    "url": "http://p1fg8xetu.bkt.clouddn.com/car_5.png",
+    "index": 5
+  },
+  {
+    "name": "保时捷",
+    "url": "http://p1fg8xetu.bkt.clouddn.com/car_6.png",
+    "index": 6
+  },
+  {
+    "name": "大众",
+    "url": "http://p1fg8xetu.bkt.clouddn.com/car_7.png",
+    "index": 7
+  },
+  {
+    "name": "哈弗",
+    "url": "http://p1fg8xetu.bkt.clouddn.com/car_8.png",
+    "index": 8
+  },
+  {
+    "name": "兰博基尼",
+    "url": "http://p1fg8xetu.bkt.clouddn.com/car_9.png",
+    "index": 9
+  },
+  {
+    "name": "雪佛兰",
+    "url": "http://p1fg8xetu.bkt.clouddn.com/car_10.png",
+    "index": 10
+  },
+  {
+    "name": "现代",
+    "url": "http://p1fg8xetu.bkt.clouddn.com/car_11.png",
+    "index": 11
+  }
+]
